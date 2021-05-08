282a168b0211a659bca03965549b51d848f2333f
Matthew Holder <holdermatthew5@gmail.com>
2021-05-07 23:16:58 -0500

    base data transfer functions

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,61 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import { useState } from 'react'
+import { getToken } from '../services/data-fetcher.js'
+
 
 export default function Home() {
-  return (
-    <div className="">
-      <Head>
-        <title>Code Fellows Rubric</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Header />
-      <main className="">
-        
-      </main>
 
-      <footer className="">
+  // State variables go here
 
-      </footer>
-    </div>
-  )
+  const [token, setToken] = useState();
+
+  // Handlers go here or in '../services/data-fetcher.js'
+
+  function loginHandler(){
+    data = getToken();
+    setToken(data.token);
+  }
+
+  function logoutHandler(){
+    url = `https://slack.com/api/auth.revoke?token=${ token }`;
+    axios.post(url);
+    setToken(null);
+  }
+
+  if (!token) {
+    return (
+      <div className="">
+        <LoginPage loginHandler={ loginHandler } />
+      </div>
+    )
+  } else {
+    return (
+      <div className="">
+        <Head>
+          <title>Code Fellows Rubric</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Header logoutHandler={ logoutHandler } />
+        <main className="">
+          
+        </main>
+
+        <footer className="">
+
+        </footer>
+      </div>
+    )
+  }
 
   function Header(props){
     return(
-    <header className="">
-      <nav>
-        <h1>Code Fellows Rubric</h1>
-        <Link href="/schedule">Schedule Student</Link>
-      </nav>
-    </header>
+      <header className="">
+        <nav>
+          <h1>Code Fellows Rubric</h1>
+          <Link href="/schedule">Schedule Student</Link>
+        </nav>
+      </header>
     )
   }
 }
