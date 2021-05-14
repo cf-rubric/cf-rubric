23c5e3285986bc7c5407dee1104a10c8ff31977e
ALAN KARLO MANGUBAT <karlomangubat92@gmail.com>
2021-05-14 09:53:10 -0700

    Merged changes and fixed styling for sign-in page

--- pages/components/login-form.js
+++ pages/components/login-form.js
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-
+import LoginHeader from './login_header'
 export default function LoginForm({ onSubmit }) {
 
     const initialValues = {
@@ -23,20 +23,22 @@ export default function LoginForm({ onSubmit }) {
     }
 
     return (
-
-        <form onSubmit={submitHandler}>
+        <div>
+        <LoginHeader />
+        <form className="w-1/3 p-10 m-auto mt-5 text-center bg-gray-200 rounded" onSubmit={submitHandler}>
             <div>
                 <label htmlFor="username">User Name</label>
-                <input type="text" name="username" id="username" value={values.username} onChange={inputChangeHandler} placeholder="User Name" />
+                <input className="w-5/6 h-10 mb-5 border-2 border-gray-500 rounded" type="text" name="username" placeholder="Username" id="username" value={values.username} onChange={inputChangeHandler} placeholder="User Name" />
             </div>
 
             <div>
                 <label htmlFor="password">Password</label>
-                <input type="password" name="password" id="password" value={values.password} onChange={inputChangeHandler} placeholder="password" />
+                <input className="w-5/6 h-10 mb-5 border-2 border-gray-500 rounded"  type="password" name="password" id="password" value={values.password} onChange={inputChangeHandler} placeholder="Password" />
             </div>
 
-            <button type="submit">Sign In</button>
+            <button className="w-5/6 h-10 transition bg-red-600 rounded hover:bg-red-500" type="submit">Sign In</button>
 
         </form>
+        </div>
     );
 }
